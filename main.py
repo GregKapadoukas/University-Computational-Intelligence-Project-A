0e9066f9b638b10404643ae62db7f58300671718
Greg Kapadoukas <gregkap12@gmail.com>
2023-04-06 04:36:11 +0300

    Added up to question A2.e, now plotting graphs, removed old model

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+import keras.backend as kb
 
 # Read CSV File With Pandas
 
@@ -82,24 +84,93 @@ test_classes = test.iloc[:,18:]
 
 # Create neural network model with keras sequencial API
 
-model = keras.Sequential(
+def CategoricalAccuracy(y_true, y_pred):
+    return kb.cast(kb.equal(kb.argmax(y_true, axis=-1), kb.argmax(y_pred, axis=-1)), kb.floatx())
+
+model1 = keras.Sequential(
     [
         keras.Input(shape=(18)),
-        layers.Dense(20, activation='relu'),
+        layers.Dense(5, activation='relu'),
         layers.Dense(5, activation='softmax')
     ]
 )
-model.summary()
+model1.summary()
 
 # Choose loss function, optimizers, learning rate and metrics
 
-model.compile(
+model1.compile(
     loss = keras.losses.CategoricalCrossentropy(),
     optimizer = keras.optimizers.Adam(learning_rate = 0.001),
-    metrics = ["accuracy"]
+    metrics = ["categorical_crossentropy", "mse", "categorical_accuracy"]
+)
+
+model2 = keras.Sequential(
+    [
+        keras.Input(shape=(18)),
+        layers.Dense(5, activation='relu'),
+        layers.Dense(5, activation='softmax')
+    ]
+)
+
+model2.summary()
+
+# Choose loss function, optimizers, learning rate and metrics
+
+model2.compile(
+    loss = keras.losses.MeanSquaredError(),
+    optimizer = keras.optimizers.Adam(learning_rate = 0.001),
+    metrics = ["categorical_crossentropy", "mse", "categorical_accuracy"]
 )
 
 # Train the model and evaluate
 
-model.fit(train_measurements, train_classes, batch_size=32, epochs=20, verbose=2)
-model.evaluate(test_measurements, test_classes, batch_size=32, verbose=2)
+num_epochs = 10
+
+model1_history = model1.fit(train_measurements, train_classes, batch_size=32, epochs=num_epochs, verbose=2)
+model2_history = model2.fit(train_measurements, train_classes, batch_size=32, epochs=num_epochs, verbose=2)
+
+print("CCE Trained Model")
+model1.evaluate(test_measurements, test_classes, batch_size=32, verbose=2)
+
+print("MSE Trained Model")
+model2.evaluate(test_measurements, test_classes, batch_size=32, verbose=2)
+
+#Plot model results
+#Compare CCE for CCE Trained and MSE Trained
+plt.plot(model1_history.history['categorical_crossentropy'])
+plt.plot(model2_history.history['categorical_crossentropy'])
+plt.title('Categorical CE vs MSE Trained NN - CCE Loss with 5 Neurons in Hidden Layer')
+plt.ylabel('CCE Loss')
+plt.xlabel('Epoch')
+x = np.arange(0, num_epochs, 1)
+#plt.xticks(x + 1)
+plt.xlim(1,num_epochs)
+plt.legend(['CCE for CCE Trained NN', 'CCE for MSE Trained NN'], loc='best')
+plt.xticks(np.arange(len(x)), np.arange(1, len(x)+1))
+plt.show()
+
+#Compare MSE for CCE Trained and MSE Trained
+plt.plot(model1_history.history['mse'])
+plt.plot(model2_history.history['mse'])
+plt.title('Categorical CE vs MSE Trained NN - MSE Loss with 5 Neurons in Hidden Layer')
+plt.ylabel('MSE Loss')
+plt.xlabel('Epoch')
+x = np.arange(0, num_epochs, 1)
+#plt.xticks(x + 1)
+plt.xlim(1,num_epochs)
+plt.legend(['MSE for CCE Trained NN', 'MSE for MSE Trained NN'], loc='best')
+plt.xticks(np.arange(len(x)), np.arange(1, len(x)+1))
+plt.show()
+
+#Compare Accuracy for CCE Trained and MSE Trained
+plt.plot(model1_history.history['categorical_accuracy'])
+plt.plot(model2_history.history['categorical_accuracy'])
+plt.title('Categorical CE vs MSE Trained NN - Accuracy with 5 Neurons in Hidden Layer')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+x = np.arange(0, num_epochs, 1)
+#plt.xticks(x + 1)
+plt.xlim(1,num_epochs)
+plt.legend(['Accuracy for CCE Trained NN', 'Accuracy for MSE Trained NN'], loc='best')
+plt.xticks(np.arange(len(x)), np.arange(1, len(x)+1))
+plt.show()
